9f5c750e119ede88e3034bc67aadfe78b8ba24b5
Alizollern <67019693+Alizollern@users.noreply.github.com>
2026-09-02 19:24:04 +0500

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,22 @@
+class MinStack:
+    def __init__(self):
+        # Стек хранит пары: (значение, минимум_на_данный_момент)
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            # Вычисляем новый минимум перед добавлением
+            current_min = min(val, self.stack[-1][1])
+            self.stack.append((val, current_min))
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
